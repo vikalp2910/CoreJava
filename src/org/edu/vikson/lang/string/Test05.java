4c010389ca12b3e43a13b29aae3424361ccf14fa
Sonkar <vikalp.sonkar@accenture.com>
2023-04-23 17:08:50 +0530

    String Programs

--- src/org/edu/vikson/lang/string/Test05.java
+++ src/org/edu/vikson/lang/string/Test05.java
@@ -3,7 +3,18 @@ package org.edu.vikson.lang.string;
 public class Test05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String s1=new String("Spring");
+		//s1=>Spring(Heap) & Spring(SCP)
+		s1.concat("Summer");
+		//summer(SCP) & SpringSummer(Heap)
+		String s2=s1.concat("Winter");
+		//Winter(SCP) & s2=>SpringWninter
+		s1=s1.concat("Fall");
+		//Fall(SCP) & s1=>SpringFall
+		
+		System.out.println(s1);  //SpringFall
+		System.out.println(s2);  //SpringWinter
 
 	}
 
